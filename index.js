7b30167fcca75b311675f58be1028292dbbb097c
zerewsg <ger62076309kyc@gmail.com>
2025-03-24 18:42:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// let greet = function(name) {
+//     console.log(`Привет, ${name}!`);
+// };
+// let calculate = function(num1, num2, callback) {
+//     return callback(num1, num2);
+// };
+
+
+
+
+
+// let add = function(a, b) {
+//     return a + b;
+// };
+// let subtract = function(a, b) {
+//     return a - b;
+// };
+// let multiply = function(a, b) {
+//     return a * b;
+// };
+// console.log(calculate(5, 3, add));      
+// console.log(calculate(5, 3, multiply)); 
+// console.log(calculate(5, 3, subtract));
+
+
+
+// function createCalculator() {
+//     return {
+//         add: function(a, b) {
+//             return a + b;
+//         },
+//         subtract: function(a, b) {
+//             return a - b;
+//         },
+//         multiply: function(a, b) {
+//             return a * b;
+//         },
+//     }
+// }
+// console.log(calculator.add(5, 3));        
+// console.log(calculator.subtract(5, 3));   
+// console.log(calculator.multiply(5, 3));
